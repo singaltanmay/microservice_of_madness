137ac3691746aabf42204ee3da9bc83022a3b0c1
Tanmay Singal <tanmaysingal@icloud.com>
2023-01-07 00:28:33 -0800

    2_24_11

---
+++ python/src/gateway/storage/util.py
@@ -0,0 +1,24 @@
+import pika, json
+
+def upload(f, fs, channel, access):
+    try:
+        fid = fs.put(f)
+    except Exception as err:
+        return "Internal Server Error", 500
+
+    message = {
+        "video_fid": str(fid),
+        "mp3_field": None,
+        "username": access["username"]
+    }
+
+    try:
+        channel.basic_public(
+            exchange="",
+            routing_key="video",
+            body=json.dumps(message),
+            properties=pika.BasicProperties(
+                delivery_mode=pika.spec.PERSISTANT_DELIVERY_MODE
+            ),
+            
+        )
